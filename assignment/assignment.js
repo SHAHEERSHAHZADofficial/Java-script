ee38fa0e9ac62950157bd22fc3c6aabd731821d5
SHAHEERSHAHZADofficial <85387726+SHAHEERSHAHZADofficial@users.noreply.github.com>
2021-06-05 12:47:09 +0500

    SHAHEERSHAHZADOFFICIAL

---
+++ assignment/assignment.js
@@ -0,0 +1,135 @@
+// var PHYSICS = parseInt( prompt("ENTER YOUR  PHYSICS MARKS"))
+// var COMPUTER = parseInt( prompt("ENTER YOUR COMPUTER MARKS"))
+// var MATH = parseInt( prompt("ENTER YOUR MATH MARKS"))
+// var totalvalue = 300
+// document.write("Total marks :"+ totalvalue + "<br>")
+// var total = PHYSICS+COMPUTER+MATH
+// alert(total)
+// document.write("Marks obtain : "+ total +"<br>")
+// var PER =total/300*100
+// alert (PER)
+// if(total >= 80){
+//    document.write("Grade : A*"+ "<br>")
+//    document.write("Remarks: Great"+ "<br>")
+// }
+// else if(total >= 70){
+//    document.write("Grade : A"+ "<br>")
+//    document.write("Remarks: Great" +"<br>")
+// }
+// else if(total >= 60){
+//    document.write("Grade : B"+ "<br>")
+//    document.write("Remarks: YOU NEED TO IMPROVE " +"<br>")
+// }
+// else if(total >= 50){
+//    document.write("Grade : FAIL"+ "<br>")
+//    document.write("Remarks: SORRY" +"<br>")
+// }
+// question 2
+// var ary = 8
+// var pr = parseInt( prompt("Enter value 1 to 10 any one"))
+// if(pr === 1){
+//    document.write (" wrong")
+// }
+// if(pr === 2){
+//    document.write (" wrong")
+// }
+// if(pr === 3){
+//    document.write (" wrong")
+// }
+// if(pr === 4){
+//    document.write (" wrong")
+// }
+// if(pr === 5){
+//    document.write ("wrong")
+// }
+// if(pr === 6){
+//    document.write ("wrong")
+// }
+// if(pr === 7){
+//    document.write ("close one")
+// }
+// if(pr === 9){
+//    document.write ("close one")
+// }
+// if(pr === 10){
+//    document.write ("wrong")
+// }
+// if(pr === 8){
+//    document.write ("correct")
+// }
+// document.write*("correct answer is " + ary)
+////question 3
+
+
+// var total = parseInt( prompt("enter todays temprature"))
+// if(total >= 40){
+
+//    document.write("Temprature is too hot")
+// }
+// else if(total >= 30){
+
+//    document.write("Temprature is normal")
+// }
+// else if(total >= 20){
+//    document.write("Temprature is cool ")
+// }
+// else if(total >= 10){
+//    document.write("Temprature is too cold")}
+
+//// question 4
+
+// var pr1 =+prompt("enter value")
+// var pr2 =prompt("enter + - * / value any one")
+// var pr3 =+prompt("enter value")
+
+
+// if( pr2 === "+" ){
+//     var result = pr1 + pr3;}
+// else if(pr2 === "*"){
+// var result = pr1 * pr3
+// }
+// else if(pr2 === "/"){
+//     var result = pr1 / pr3
+//     }
+//     else if(pr2 === "-"){
+//         var result = pr1 - pr3
+//         }
+//         var out = pr1 + pr2 + pr3 + "="+result
+//         document.write(out)
+
+// question  5
+
+
+// function get (){
+//    var password = document.getElementById("pass")
+// var passconf = document.getElementById("passcon")
+// if(password==="")
+// {
+//    alert("password is required")
+// }
+// else if(passconf === password)
+// {
+//    alert("password is correct")
+// }
+// else{ alert("password is not correct")}
+// }
+// // question 6
+// var array = ["Apple",
+//    "Samsung", "Motorola", "Nokia", "Sony" , "Haier" ];
+//    document.write(array[0],"<br>")
+//    document.write(array[1],"<br>")
+//    document.write(array[2],"<br>")
+//    document.write(array[3],"<br>")
+//    document.write(array[4],"<br>")
+//    document.write(array[5],"<br>")
+
+// // question 7 
+// var number = parseInt(prompt('Enter an integer: '));
+// var num =parseInt(prompt("enter your limit"))
+
+// for(let i = 1; i <= num; i++ ) {
+
+
+//     var result = i * number;
+//     document.write(number +"&nbsp"+"x" +"&nbsp"+i +"&nbsp"+"=" +"&nbsp"+result +"<br>")
+// }
